fc3fbf16d7b0ffa91b88403194c99d89f0c15b96
Varun Krishna <varunkrishnachess@gmail.com>
2023-04-07 15:24:31 +0530

    updated our-services component

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,17 @@
 import NavBar from "../NavBar";
 import "./home.css";
+import navImage from "../../images/simon-lee-HmNmpnIZlU0-unsplash.png";
+import contentImage from "../../images/planet-volumes-Olu4k8c4A5Q-unsplash.png";
 
 const Home = () => {
     return ( 
         <>
             <NavBar />
+
+            {/* Below NavBar */}
+
             <div className="nav-container">
+
                 <div className="nav-content">
                     <h1 className="nav-heading">ESG made easy for you.</h1>
                     <p className="nav-content">
@@ -16,7 +22,76 @@ const Home = () => {
                     </p>
                     <button className="nav-button">Get Started</button>
                 </div>
+
+                <div className="nav-image-container">
+                    <img classname="nav-image" src={navImage}></img>
+                </div>
             </div>
+
+            {/* Our Services component */}
+
+            <div className="our-services-container">
+                <h1 className="our-services">OUR SERVICES</h1>
+
+                <div className="our-services-content-container">
+
+                    <div className="our-services-image">
+                        <h1>How We Add Value</h1>
+                        <img classname="services-image" src={contentImage}></img>
+
+                    </div>
+
+                    <div className="our-services-content">
+                        <h2>Project Management</h2>
+                        <p>
+                           Using state of the art project management tools, processes and strategic 
+                           planning every step of the way, we ensure optimum resource utilisation, 
+                           effective communication and collaboration between stakeholders, 
+                           achieving intended objectives on schedule and on budget.
+                        </p> 
+
+                        <a href="#">Learn More</a>
+
+                        <h2>Project Management</h2>
+                        <p>
+                           Using state of the art project management tools, processes and strategic 
+                           planning every step of the way, we ensure optimum resource utilisation, 
+                           effective communication and collaboration between stakeholders, 
+                           achieving intended objectives on schedule and on budget.
+                        </p> 
+
+                        <a href="#">Learn More</a>
+
+                        <h2>Project Management</h2>
+                        <p>
+                           Using state of the art project management tools, processes and strategic 
+                           planning every step of the way, we ensure optimum resource utilisation, 
+                           effective communication and collaboration between stakeholders, 
+                           achieving intended objectives on schedule and on budget.
+                        </p> 
+
+                        <a href="#">Learn More</a>
+
+                        <h2>Project Management</h2>
+                        <p>
+                           Using state of the art project management tools, processes and strategic 
+                           planning every step of the way, we ensure optimum resource utilisation, 
+                           effective communication and collaboration between stakeholders, 
+                           achieving intended objectives on schedule and on budget.
+                        </p> 
+
+                        <a href="#">Learn More</a>
+                    </div>
+
+                </div>
+            </div>
+
+            {/* Manage Text */}
+
+            <div className="manage-text">
+                <h1>Manage your CSR & ESG investments through a single platform</h1>
+            </div>
+
         </>
      );
 }
