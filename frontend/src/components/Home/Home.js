7207bcb0b9566dee56ea01aec5867c7afb065161
Varun Krishna <varunkrishnachess@gmail.com>
2023-04-08 08:27:12 +0530

    basic card layout added

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -89,7 +89,81 @@ const Home = () => {
             {/* Manage Text */}
 
             <div className="manage-text">
-                <h1>Manage your CSR & ESG investments through a single platform</h1>
+                <h1>Manage your <span className="italics">CSR & ESG investments</span> through a single platform</h1>
+            </div>
+
+            {/* Projects */}
+
+            {/* <div className="projects-outer">
+                hello
+            </div> */}
+
+            {/* Sign Up and Login Sections */}
+
+            <div className="signup-login-section">
+                <h1>Essensa’s all-in-one web based portfolio management system </h1>
+                <div className="sign-log-btns">
+                    <a className="signup-btn">Sign Up</a>
+                    <button className="login-btn" href="#">Login</button>
+                </div>
+            </div>
+
+
+            {/* Contact Section */}
+
+            <div className="contact-container">
+                <div className="contact-info">
+                    <h1>Contact Us</h1>
+
+                    <p>hi@essensa.com</p>
+                    <p>Location</p>
+                </div>
+                 
+                <div className="contact-form">
+                    hello
+                </div> 
+            </div>
+
+            {/* Subscription */}
+
+            <div className="subscription-plans-container">
+                <div className="choose-right-plan">
+                    <h1>Choose the Right Plan <span className="italics"> for Your CSR and ESG Investment Goals </span> </h1>
+                </div>
+
+
+                <div classname="plans-container">
+                    <div className="card-1">
+                        <h1>3 Months</h1>
+                        <p className="service-tag">Services Included:</p>
+                        <p className="duties-tag">Consulting,Auditing & Web Platform</p>
+
+                        <h6>Tier-1 Companies</h6>
+                        <h2>1.5 Lakhs</h2>
+
+                        <h5>Tier-1 Companies</h5>
+                        <h3>3.3 Lakhs</h3>
+
+                        <button>Subscribe</button>
+                    </div>
+
+                    {/* <div className="card-2">
+                        <h1>3 Months</h1>
+                        <p className="service-tag">Services Included:</p>
+                        <p className="duties-tag">Consulting,Auditing & Web Platform</p>
+
+                        <h6>Tier-1 Companies</h6>
+                        <h2>1.5 Lakhs</h2>
+
+                        <h5>Tier-1 Companies</h5>
+                        <h3>3.3 Lakhs</h3>
+
+                        <button>Subscribe</button>
+                    </div> */}
+
+
+                </div>
+
             </div>
 
         </>
