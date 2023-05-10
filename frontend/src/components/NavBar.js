68d57829a387fe160e44b90bf569e39dcf453de0
Varun Krishna <varunkrishnachess@gmail.com>
2023-05-10 12:37:20 +0530

    navbar updated

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,16 +4,19 @@ import logo from "../images/logo-nav.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <img src={logo} />
-      <ul className="navbar-list">
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Pricing</a></li>
-        <li><a href="#">About Us</a></li>
-        <li className='login'><a href="/login">Log In</a></li>
-        <li><button className="get_in_touch-button">Get In Touch</button></li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="navbar flex justify-between align-center px-16 py-128 w-full h-20 bg-custom-light-green4">
+        <img src={logo} />
+        <ul className="navbar-list">
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">About Us</a></li>
+          <li className='login'><a href="/login">Log In</a></li>
+          <li><button className="get_in_touch-button">Get In Touch</button></li>
+        </ul>
+      </nav>
+    </div>
+
   );
 }
 
